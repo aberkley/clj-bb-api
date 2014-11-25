f63ca059185119176009032132847ee1172e3291
aberkley <alexander.berkley@googlemail.com>
2014-11-25 15:33:41 +0000

    added nexus settings to project

--- project.clj
+++ project.clj
@@ -5,4 +5,11 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [tulos/bberg-sdk "3.6.1.0"]
-                 [clj-time "0.6.0"]])
+                 [clj-time "0.6.0"]]
+  :mirrors {#".+" {:name "nexus", :url "http://ec2-54-186-140-249.us-west-2.compute.amazonaws.com:8081/nexus/content/groups/public/"}}
+  :repositories [["releases" {:url "http://ec2-54-186-140-249.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/releases/"
+                              :username :env
+                              :password :env }]
+                 ["snapshots" {:url "http://ec2-54-186-140-249.us-west-2.compute.amazonaws.com:8081/nexus/content/repositories/snapshots/"
+                               :username :env
+                               :password :env }]])
