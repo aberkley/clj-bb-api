b2f41818370b6be74ffa872266095585168cf83b
aberkley <alexander.berkley@googlemail.com>
2014-11-25 15:40:01 +0000

    changed project namespace

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-bb-api "0.1.0-SNAPSHOT"
+(defproject sra-london/clj-bb-api "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
