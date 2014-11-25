74276c399c6d81ec482aaac403830c6f186b8e6f
aberkley <alexander.berkley@googlemail.com>
2014-11-25 14:55:31 +0000

    initialise project

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clj-bb-api
+
+TODO: write [great documentation](http://jacobian.org/writing/great-documentation/what-to-write/)
---
+++ test/clj_bb_api/core_test.clj
@@ -0,0 +1,7 @@
+(ns clj-bb-api.core-test
+  (:require [clojure.test :refer :all]
+            [clj-bb-api.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
